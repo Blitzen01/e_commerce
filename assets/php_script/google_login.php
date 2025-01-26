305c2fd5821384f019c0502e374b9d1b31498ef3
Blitzen01 <ronjustin22@gmail.com>
2025-01-26 16:26:07 +0800

    google auth

--- assets/php_script/google_login.php
+++ assets/php_script/google_login.php
@@ -4,10 +4,17 @@ require_once 'vendor/autoload.php';  // Include the Google Client library
 // Start a session to store user data
 session_start();
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Access the secrets
+$clientId = getenv('CLIENT_ID');
+$clientSecret = getenv('SECRET_TOKEN');
+
 // Create the Google client object
 $client = new Google_Client();
-$client->setClientId('871355226961-19ip522e0enk01oofpqdbscedb6r2ini.apps.googleusercontent.com');
-$client->setClientSecret('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');
+$client->setClientId('$clientId');
+$client->setClientSecret('$clientSecret');
 $client->setRedirectUri('http://localhost/e_commerce/user/index.php'); // Use the URI where Google will redirect after login
 $client->addScope('email');  // Request email access
 
