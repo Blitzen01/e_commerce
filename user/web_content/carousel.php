b7715c265f473fdec2f3a0c1b5db34f8106851ab
Blitzen01 <ronjustin22@gmail.com>
2025-04-26 20:47:37 +0800

    carousel

--- user/web_content/carousel.php
+++ user/web_content/carousel.php
@@ -1,19 +1,26 @@
+<style>
+#continuousCarousel .carousel-inner {
+  transition: transform 2s linear;
+}
+</style>
+
+
 <div id="carousel_display" class="carousel-container mb-3" style="<?php echo $carouselStyle; ?>">
-    <div id="continuousCarousel" class="carousel slide shadow border" data-bs-ride="carousel" data-bs-interval="3000">
+    <div id="continuousCarousel" class="carousel slide shadow border overflow-hidden" data-bs-ride="carousel" data-bs-interval="2000">
         <div class="carousel-inner">
             <?php
             $sql = "SELECT * FROM carousel";
             $result = mysqli_query($conn, $sql);
 
             if ($result) {
-                $isActive = true; // To set the "active" class only on the first item
+                $isActive = true;
                 while ($row = mysqli_fetch_assoc($result)) {
                     ?>
                     <div class="carousel-item <?php if ($isActive) echo 'active'; ?>">
                         <img src="../assets/image/carousel/<?php echo $row['img_name']; ?>" class="d-block w-100" alt="">
                     </div>
                     <?php
-                    $isActive = false; // Ensure only the first item has the "active" class
+                    $isActive = false;
                 }
             }
             ?>
